3065f9903795276d33d627c312a6b5c20ccfd690
ehachmann <ehachmann@hotmail.com>
2025-04-03 21:21:57 -0300

    Converter a temperatura da escala Celsius para a escala Fahrenheit

---
+++ ListaDeExercicios.Exercicio04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ListaDeExercicios.Exercicio04
+{
+    internal class Program
+    {
+        // Converter a temperatura da escala Celsius para a escala Fahrenheit
+        static void Main(string[] args)
+        {
+            #region input
+            Console.Write("Digite a temperatura que deseja converter de Celsius para Fahrenheit: ");
+            double celsius = double.Parse(Console.ReadLine());
+            #endregion
+
+            #region process
+            double fahrenheit = (celsius * 1.8) + 32;
+            #endregion
+
+            #region output
+            Console.WriteLine();
+            Console.WriteLine($"A temperatura {celsius}°C é equivalente a {fahrenheit}°F.");
+            Console.WriteLine();
+            Console.WriteLine("Pressione ENTER para sair...");
+            Console.Read();
+            #endregion
+
+        }
+    }
+}
