0599e345e0bbc18070f1ca2e2cb22ef1b054650e
ehachmann <ehachmann@hotmail.com>
2025-04-03 21:00:42 -0300

    Add project files.

---
+++ ListaDeExercicios.Exercicio01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListaDeExercicios.Exercicio01
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o volume de uma caixa retangular
+        //VOLUME_DA_CAIXA = COMPRIMENTO * LARGURA * ALTURA
+        static void Main(string[] args)
+        {
+            #region input de dados
+
+            Console.WriteLine("Digite o comprimento da caixa: ");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a largura da caixa: ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura da caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+            #endregion
+
+            #region processamento
+            decimal resultado = comprimento * largura * altura;
+            #endregion
+
+            #region output dos dados
+            Console.WriteLine($"O volume da caixa é de {resultado}.");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListaDeExercicios.Exercicio03/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ListaDeExercicios.Exercicio03
+{
+    internal class Program
+    {
+        // Crie um programa para calcular o consumo de combustível por quilometro percorrido
+        static void Main(string[] args)
+        {
+            // input de dados
+
+            Console.Write("Digite a quilometragem atual do veículo: ");
+            int quilometragemInicial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite a quilometragem final da viagem: ");
+            int quilometragemFinal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de combustível consumida na viagem: ");
+            int quantidadeCombustivel = int.Parse(Console.ReadLine());
+
+            // processamento
+
+            int consumo = (quilometragemFinal - quilometragemInicial) / quantidadeCombustivel;
+
+            // output
+
+            Console.WriteLine();
+            Console.WriteLine($"O consumo de combustível por quilômetro percorrido é: {consumo} km/l.");
+            Console.WriteLine();
+            Console.WriteLine("Pressione ENTER para sair...");
+            Console.Read();
+
+        }
+    }
+}
