77cfac5b3283adacd22834f67c28c4ac593fb5bf
ehachmann <ehachmann@hotmail.com>
2025-04-03 21:54:04 -0300

    Calcular o salário total de um vendedor.

---
+++ ListaDeExercicios.Exercicio05/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListaDeExercicios.Exercicio05
+{
+    internal class Program
+    {
+        // Calcular o salário total de um vendedor.
+        static void Main(string[] args)
+        {
+            #region imput
+            Console.Write("Digite o salario base do funcionário: R$ ");
+            double salarioBase = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o valor total de vendas do funcionário: R$ ");
+            double valorVendas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o percentual de comissão do funcionário: % ");
+            double percentualComissao = double.Parse(Console.ReadLine());
+            #endregion
+
+            #region processamento  
+            double comissao = (valorVendas * percentualComissao) / 100;
+            double salarioTotal = salarioBase + comissao;
+            #endregion
+
+            #region output
+            Console.WriteLine();
+            Console.WriteLine($"O salário total do vendedor é R$ {salarioTotal} ");
+            Console.ReadKey();
+            #endregion             
+        }
+    }
+}
