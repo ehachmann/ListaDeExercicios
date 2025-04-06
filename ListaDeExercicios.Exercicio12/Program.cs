0d828fdd4aded7fa5b94b7c8e54068244270cd92
ehachmann <ehachmann@hotmail.com>
2025-04-06 15:55:23 -0300

    Gerar e escreve os números ímpares entre 100 e 200

---
+++ ListaDeExercicios.Exercicio12/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ListaDeExercicios.Exercicio12
+{
+    internal class Program
+    {
+        // Gerar e escreve os números ímpares entre 100 e 200
+        static void Main(string[] args)
+        {
+            #region input
+            Console.WriteLine("Números Ímpares entre 100 e 200");
+            Console.WriteLine();
+            #endregion
+
+            #region process
+            for (int i = 100; i < 200; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write($"{i} - ");
+                }
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
