c69639d93e35e0be1de7e8a462e78482d13bbbcb
ehachmann <ehachmann@hotmail.com>
2025-04-06 15:26:39 -0300

    Calcular o IMC - Índice de Massa Corporal

---
+++ ListaDeExercicios.Exercicio10/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ListaDeExercicios.Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calcular o IMC - Índice de Massa Corporal
+
+            string resultado;
+
+            #region input
+            Console.Write("Informe o peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a altura: ");
+            double altura = double.Parse(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            double imc = peso / Math.Pow(altura, 2);
+
+            if (imc < 18.5)
+            {
+                resultado = "Abaixo do peso";
+            }
+            else if (imc >= 18.5 && imc <= 25)
+            {
+                resultado = "Peso normal";
+            }
+            else if (imc > 25 && imc <= 30)
+            {
+                resultado = "Acima do Peso";
+            }
+            else
+            {
+                resultado = "Obeso";
+            }
+            #endregion
+
+            #region output
+            Console.WriteLine();
+            Console.WriteLine($"O IMC é: {imc}");
+            Console.WriteLine();
+            Console.WriteLine($"Classificação: {resultado}");
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
